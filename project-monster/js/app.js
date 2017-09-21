0e5c9b04cd09ec1e962445025361abfe597906f0
Ronalson Filho <contact@ronalson.com.br>
2017-09-21 16:04:04 -0300

    Add methods for attack, specialAttack and heal

--- project-monster/js/app.js
+++ project-monster/js/app.js
@@ -5,6 +5,23 @@ new Vue({
     usersHealth: 100,
     usersMana: 5,
     monsterHealth: 100
+  },
+  methods: {
+    damage() {
+      return Math.floor(Math.random() * 10) + 1
+    },
+    attack() {
+      this.usersHealth = this.usersHealth - this.damage();
+      this.monsterHealth = this.monsterHealth - this.damage();
+    },
+    specialAttack() {
+      this.monsterHealth = this.monsterHealth - (3 * this.damage());
+      this.usersMana--;
+    },
+    heal() {
+      this.usersHealth = this.usersHealth + this.damage();
+      this.usersMana--;
+    }
   }
 });
 
