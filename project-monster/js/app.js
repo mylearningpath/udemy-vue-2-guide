c4d7a7e21e80e12eaaebcc88c36c9f5ff9ca75b0
Ronalson Filho <contact@ronalson.com.br>
2017-09-21 17:49:38 -0300

    Refactor battle logic to avoid repetitions

--- project-monster/js/app.js
+++ project-monster/js/app.js
@@ -2,43 +2,82 @@ new Vue({
   el: '#app',
   data: {
     gameIsRunning: false,
-    usersHealth: 100,
-    usersMana: 5,
+    playerHealth: 100,
     monsterHealth: 100,
+    playerMana: 5,
     battleRegistry: []
   },
   methods: {
     startGame() {
       this.gameIsRunning = true;
-      this.usersHealth = 100;
+      this.playerHealth = 100;
       this.monsterHealth = 100;
       this.battleRegistry = []
     },
-    randomPoints() {
-      return (Math.floor(Math.random() * 10) + 1)
-    },
-    usersDamage() {
-      let damage = this.randomPoints();
-      this.usersHealth -= damage;
-      this.battleRegistry.push({ monsterAttack: damage });
-    },
-    monsterDamage(multiplier = 1) {
-      let damage = this.randomPoints() * multiplier;
-      this.monsterHealth -= damage;
-      this.battleRegistry.push({ userAttack: damage });
-    },
     attack() {
-      this.usersDamage();
-      this.monsterDamage();
+      let damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage;
+      this.battleRegistry.push({ playerAttack: damage });
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttacks();
+
     },
     specialAttack() {
-      this.usersDamage();
-      this.monsterDamage(3);
-      this.usersMana--;
+      let damage = this.calculateDamage(5, 20);
+      this.monsterHealth -= damage;
+      this.battleRegistry.push({ playerAttack: damage });
+      this.playerMana--;
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttacks();
     },
     heal() {
-      this.usersHealth += this.randomPoints();
-      this.usersMana--;
+      if (this.playerHealth <= 85) {
+        this.playerHealth += 15;
+      } else {
+        this.playerHealth = 100;
+      }
+
+      this.playerMana--;
+
+      this.monsterAttacks();
+    },
+    giveUp() {
+      this.gameIsRunning = false;
+    },
+    monsterAttacks() {
+      let damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.battleRegistry.push({ monsterAttack: damage });
+      this.checkWin();
+    },
+    calculateDamage(min, max) {
+      return Math.max((Math.floor(Math.random() * max) + 1), min);
+    },
+    checkWin() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New game?')) {
+          this.startGame();
+        } else {
+            this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost! Try again?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
     }
   }
 });
