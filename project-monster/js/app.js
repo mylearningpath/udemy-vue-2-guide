23f5cad2e7b1ca887376dc532b0e8740128af120
Ronalson Filho <contact@ronalson.com.br>
2017-09-21 16:48:04 -0300

    Refactor damage logic and implement turn registry.

--- project-monster/js/app.js
+++ project-monster/js/app.js
@@ -4,26 +4,35 @@ new Vue({
     gameStarted: false,
     usersHealth: 100,
     usersMana: 5,
-    monsterHealth: 100
+    monsterHealth: 100,
+    battleRegistry: []
   },
   methods: {
-    damage() {
-      return Math.floor(Math.random() * 10) + 1
+    randomPoints() {
+      return (Math.floor(Math.random() * 10) + 1)
+    },
+    usersDamage() {
+      let damage = this.randomPoints();
+      this.usersHealth -= damage;
+      this.battleRegistry.push({ monsterAttack: damage });
+    },
+    monsterDamage(multiplier = 1) {
+      let damage = this.randomPoints() * multiplier;
+      this.monsterHealth -= damage;
+      this.battleRegistry.push({ userAttack: damage });
     },
     attack() {
-      this.usersHealth = this.usersHealth - this.damage();
-      this.monsterHealth = this.monsterHealth - this.damage();
+      this.usersDamage();
+      this.monsterDamage();
     },
     specialAttack() {
-      this.monsterHealth = this.monsterHealth - (3 * this.damage());
+      this.usersDamage();
+      this.monsterDamage(3);
       this.usersMana--;
     },
     heal() {
-      this.usersHealth = this.usersHealth + this.damage();
+      this.usersHealth += this.randomPoints();
       this.usersMana--;
     }
   }
 });
-
-
-// SUDO CODE
